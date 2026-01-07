84ff4bcc432a14124b48dcf8dce028c85d353a0f
Imaginbright <brightified2004@gmail.com>
2026-01-07 17:58:09 +0100

    Implemented max width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navigation/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-[1600px] mx-auto w-full">
       <Navbar />
       <PortfolioGrid />
     </div>
